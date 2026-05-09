148559c0c63450fdb5f017ed4a227b9315e652e5
Suyash-Vyas24 <suyashvyas2404@gmail.com>
2026-05-09 08:50:43 +0530

    Cleaned up next config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true }
 };
-
-module.exports = nextConfig; // If your file ends in .mjs, use 'export default nextConfig;' instead
+module.exports = nextConfig;
